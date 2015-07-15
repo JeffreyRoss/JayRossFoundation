5f7ce3edca7b34706ad1668284b203535bb38fc0
JeffreyRoss <jeffrey1.ross@gmail.com>
2015-07-15 00:45:51 -0400

    adding baseline controllers and views, refining navigation and subnavigation styling

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,19 @@
+class MainController < ApplicationController
+  def home
+  end
+
+  def aboutus
+  end 
+
+  def initiatives
+  end
+
+  def takeaction
+  end
+
+  def ourimpact
+  end
+
+  def contact
+  end
+end
