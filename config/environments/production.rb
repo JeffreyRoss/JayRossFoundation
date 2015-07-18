e470c29af15d3dcd3983a109c161f31e86166287
JeffreyRoss <jeffrey1.ross@gmail.com>
2015-07-17 22:09:36 -0400

    experimenting with heroku assets config settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   #heroku platform static assets config setting
   # see https://devcenter.heroku.com/articles/ruby-support#injected-plugins
