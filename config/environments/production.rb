e9e3e2f80f538fa1ed5c827fb7df5266027fd507
JeffreyRoss <jeffrey1.ross@gmail.com>
2015-09-26 10:01:37 -0400

    removed depreciated rails config config.serve_static_assets and replaced with config.serve_static_files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
 
   #heroku platform static assets config setting
   # see https://devcenter.heroku.com/articles/ruby-support#injected-plugins
-  config.serve_static_assets = true
+  config.serve_static_files = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
